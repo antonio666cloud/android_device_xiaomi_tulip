e5ab98a34a6cd52030bd16602c1fb59808d13ea4
antonio666cloud <68087987+antonio666cloud@users.noreply.github.com>
2020-07-19 01:57:03 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_twolip.mk
+    $(LOCAL_DIR)/bliss_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_twolip-user \
-    lineage_twolip-userdebug \
-    lineage_twolip-eng
+    bliss_tulip-user \
+    bliss_tulip-userdebug \
+    bliss_tulip-eng
